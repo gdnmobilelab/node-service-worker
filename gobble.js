6d11e5a921b303353d91a41dafe57d53ee55f328
Alastair Coote <alastair.coote@gmail.com>
2017-10-20 11:20:46 -0400

    get rid of debug messages

--- gobble.js
+++ gobble.js
@@ -1,88 +1,93 @@
-const {ServiceWorker, installAndActivate, FetchEvent} = require('./index');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
+const { ServiceWorker, installAndActivate, FetchEvent } = require("./index");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = function(input, output, options, callback) {
-   
-    let entryHasChanged = this.changes.find((c) => c.file === options.entry);
+  let entryHasChanged = this.changes.find(c => c.file === options.entry);
 
-    let jsFile = path.join(input, options.entry);
-    let scopePath = url.parse(options.scope).pathname;
-    
-    this.sander.readdir(input)
-    .then((entries) => {
-        let cpPromises = entries.map((e) => this.sander.symlinkOrCopy(input, e).to(output, e))
-        return Promise.all(cpPromises)
+  let jsFile = path.join(input, options.entry);
+  let scopePath = url.parse(options.scope).pathname;
+
+  this.sander
+    .readdir(input)
+    .then(entries => {
+      let cpPromises = entries.map(e =>
+        this.sander.symlinkOrCopy(input, e).to(output, e)
+      );
+      return Promise.all(cpPromises);
     })
     // this.sander.symlinkOrCopy(input).to(output)
     .then(() => {
-        // if (!entryHasChanged) {
-        //     console.log("Skipping rebuild as entry has not changed")
-        //     return false;
-        // }
-        console.log("Processing new service worker file...")
-        return this.sander.readFile(jsFile, {encoding:'UTF-8'})
-        .then((contents) => {
+      // if (!entryHasChanged) {
+      //     console.log("Skipping rebuild as entry has not changed")
+      //     return false;
+      // }
+      return this.sander
+        .readFile(jsFile, { encoding: "UTF-8" })
+        .then(contents => {
+          let worker = new ServiceWorker({
+            scriptURL: url.resolve(options.scope, "/" + options.entry),
+            scope: options.scope,
+            contents: contents,
+            importScript: function(url) {
+              let entryDir = path.dirname(options.entry);
+              let relativeToScript = path.relative(options.entry, url);
+              return fs.readFileSync(path.join(input, entryDir, url));
+            }
+          });
+          return installAndActivate(worker).then(() => {
+            let absoluteURLsToFetch = options.urls.map(u =>
+              url.resolve(options.scope, u)
+            );
+            let fetchPromises = absoluteURLsToFetch.map(u => {
+              let fetchEvent = new FetchEvent(u);
+              worker.dispatchEvent(fetchEvent);
 
-            let worker = new ServiceWorker({
-                scriptURL: url.resolve(options.scope, "/" + options.entry),
-                scope: options.scope,
-                contents: contents,
-                importScript: function(url) {
-                    let entryDir = path.dirname(options.entry)
-                    let relativeToScript = path.relative(options.entry, url);
-                    return fs.readFileSync(path.join(input,entryDir, url));
-                }
-            });
-            return installAndActivate(worker)
-            .then(() => {
-                let absoluteURLsToFetch = options.urls.map((u) => url.resolve(options.scope, u));
-                let fetchPromises = absoluteURLsToFetch.map((u) => {
+              return fetchEvent
+                .resolve()
+                .catch(err => {
+                  console.error("Encoutered worker in worker.");
+                  worker.globalScope.console.dump();
+                  throw err;
+                })
+                .then(res => {
+                  let parsedURL = url.parse(u);
+                  let resolveBackToScope = path.relative(
+                    scopePath,
+                    parsedURL.path
+                  );
 
-                    let fetchEvent = new FetchEvent(u);
-                    worker.dispatchEvent(fetchEvent);
+                  // path.relative strips out trailing /, but we want it
+                  if (
+                    resolveBackToScope !== "" &&
+                    parsedURL.path.substr(-1) === "/"
+                  ) {
+                    resolveBackToScope += "/";
+                  }
 
-                    return fetchEvent.resolve()
-                    .catch((err) => {
-                        console.error("Encoutered worker in worker.");
-                        worker.globalScope.console.dump();
-                        throw err;
-                    })
-                    .then((res) => {
-                        worker.globalScope.console.dump();
-                        let parsedURL = url.parse(u);
-                        let resolveBackToScope = path.relative(scopePath, parsedURL.path);
-                    
-                        // path.relative strips out trailing /, but we want it
-                        if (resolveBackToScope !== "" && parsedURL.path.substr(-1) === '/') {
-                            resolveBackToScope += '/';
-                        }
-
-                        if (resolveBackToScope.lastIndexOf("/") == resolveBackToScope.length -1) {
-                            resolveBackToScope += "index.html";
-                        }
-                        console.log('scope?', resolveBackToScope)
-                        let toWriteTo = path.resolve(output, resolveBackToScope);
-                        return res.buffer()
-                        .then((buffer) => {
-                            console.log("write to", toWriteTo)
-                            return this.sander.writeFile(toWriteTo, buffer);
-                        });
-                    })
-                    
+                  if (
+                    resolveBackToScope.lastIndexOf("/") ==
+                    resolveBackToScope.length - 1
+                  ) {
+                    resolveBackToScope += "index.html";
+                  }
+                  let toWriteTo = path.resolve(output, resolveBackToScope);
+                  return res.buffer().then(buffer => {
+                    return this.sander.writeFile(toWriteTo, buffer);
+                  });
                 });
-                return Promise.all(fetchPromises);
-            })
-        })
+            });
+            return Promise.all(fetchPromises);
+          });
+        });
     })
-   
-    .then((responses) => {
-        callback();
+    .then(responses => {
+      callback();
     })
-    .catch((err) => {
-        callback(err);
-    })
+    .catch(err => {
+      callback(err);
+    });
 };
 
 module.exports.id = "node-service-worker";
